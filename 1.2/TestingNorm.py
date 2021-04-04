3c0335b6e79c305b5fbe49eff58b072780f51c75
HarmanDotpy <harmansingh.iitd@gmail.com>
2021-04-04 15:55:05 +0530

    Group Norm done and tested. With this all implementations of norms are complete

--- 1.2/TestingNorm.py
+++ 1.2/TestingNorm.py
@@ -5,6 +5,7 @@ from InstanceNorm import InstanceNorm2D
 from BatchNorm import BatchNorm2D
 from LayerNorm import LayerNorm2D
 from BatchInstanceNorm import BIN2D
+from GroupNorm import GroupNorm2D
 
 
 class NormTestCase(unittest.TestCase):
@@ -52,6 +53,26 @@ class NormTestCase(unittest.TestCase):
             print(output)
             # print((output - output_pytorch).mean())
             # self.assertTrue((output - output_pytorch).mean()< 1e-7)
+
+    def test_GroupNorm2D(self):
+        print("GROUPNORM--------------------------------")
+        m = GroupNorm2D(3, 6)
+        m_pytorch = nn.GroupNorm(3, 6)
+        for i in range(1, 10):
+            input = torch.randn(3, 6, i, i)
+            output_pytorch = m_pytorch(input)
+            output = m(input)
+            print((output - output_pytorch).mean())
+            self.assertTrue((output - output_pytorch).mean()< 1e-7)
+        #make groups of 1
+        m = GroupNorm2D(1, 6)
+        m_pytorch = nn.GroupNorm(1, 6)
+        for i in range(1, 10):
+            input = torch.randn(3, 6, i, i)
+            output_pytorch = m_pytorch(input)
+            output = m(input)
+            print((output - output_pytorch).mean())
+            self.assertTrue((output - output_pytorch).mean()< 1e-7)
 
 if __name__ == '__main__':
     unittest.main()
