35dc51020b7810edca72d7869351274df8f0a67f
Shubham Mittal <65343158+sm354@users.noreply.github.com>
2021-04-20 23:56:32 +0530

    deleting old (unstructured) files

---
+++ install_requirements.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+conda install scikit-learn=0.23.2
+conda install matplotlib=3.2.2
+conda install numpy=1.16.4
+conda install pytorch=1.1.0
+conda install torchvision=0.3.0
+conda install pandas=1.1.3
